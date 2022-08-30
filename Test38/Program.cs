1bf83447b59ec0f31c1e2c3ab3973ad362d4c374
anzhelaas <109167847+anzhelaas@users.noreply.github.com>
2022-08-30 15:38:43 +0300

    Сделала домашнее задание

---
+++ Test38/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и
+// минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+Console.Clear();
+
+double[] array = {3, 7, 22, 2, 78};
+
+double count = 0;
+double max = 0;
+double min = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+count = max - min;
+Console.WriteLine(string.Join(" , ", array));
+Console.WriteLine($"Минимальное число = {min} ");
+Console.WriteLine($"Максимальное число = {max} ");
+Console.WriteLine($"Разница между максимальным применением и значением = {count} ");
